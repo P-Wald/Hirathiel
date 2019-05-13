957cf3d8db3b671e03ae7cb121ca72ed868cecf1
pwald <philip.wald@gmail.com>
2019-05-13 14:31:00 +0200

    Added a basic template for Lists, Generic adapter to properly work with objects is still required

---
+++ Hirathiel/Hirathiel/Stack.hpp
@@ -0,0 +1,85 @@
+#pragma once
+#include "Stack.hpp"
+#include <vector>
+#include <cstdlib>
+#include <iostream>
+
+
+template <class T>
+class Stack {
+public:
+	void add(T element);
+	void remove(T element);
+	bool empty() { return this->elements.empty(); }
+	std::vector<T> get() { return this->elements; }
+	void forEach(void (*ptr)(T element));
+	T* getObj(int index);
+	T getCopy(int index);
+	int getIndex(T element);
+
+private:
+	std::vector<T> elements;
+
+};
+
+
+template <class T>
+void Stack<T>::add(T element) {
+	this->elements.push_back(element);
+}
+
+template<class T>
+void Stack<T>::remove(T element) {
+	if (element) {
+		int y = 0;
+		for (auto i = this->elements.begin(); i != this->elements.end(); ++i) {
+			if (this->elements[y] == element) {
+				this->elements.erase(i);
+				break;
+			}
+			y++;
+		}
+	}
+}
+
+
+template<class T>
+int Stack<T>::getIndex(T element) {
+	if (element) {
+		int y = 0;
+		for (auto i = this->elements.begin(); i != this->elements.end(); ++i) {
+			if (this->elements[y] == element) {
+				return y;
+			}
+			y++;
+		}
+	}
+}
+
+template<class T>
+void Stack<T>::forEach(void(*ptr)(T element)) {
+	for (int i = 0; i < this->elements.size(); i++) {
+		(*ptr)(this->elements[i]);
+	}
+	std::cout << std::endl;
+}
+
+template<class T>
+T* Stack<T>::getObj(int index) {
+	if (index >= 0 && index < this->elements.size()) {
+		return *elements[index];
+	}
+	else {
+		return NULL;
+	}
+}
+
+template<class T>
+T Stack<T>::getCopy(int index) {
+	if (index >= 0 && index < this->elements.size()) {
+		return elements[index];
+	}
+	else {
+		return NULL;
+	}
+}
